55b15eee40b59cf68ce1da0078622cad7712dc25
amandobarbosa <amandobarbosa7@gmail.com>
2024-10-29 16:00:11 -0300

    tipo void

---
+++ src/A0003-tipo-void/A0003-tipo-void.js
@@ -0,0 +1,14 @@
+function semRetorno(...args: string[]): void{
+console.log(args.join(' '))
+}
+
+const pessoa = {
+    nome: "Amando",
+    sobrenome: "Barbosa",
+
+    exibirNome(): void{
+        console.log(this.nome + '' + this.sobrenome)
+    }
+}
+
+semRetorno('Amando', 'barbosa')
